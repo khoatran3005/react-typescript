1ed2199744c7ed318751600baebe7e0e70c47a4c
Khoa Tran <khoatran3005@gmail.com>
2023-05-08 09:06:06 -0400

    split component

--- src/examples/Video7.tsx
+++
@@ -1,7 +0,0 @@
-const Video7: React.FC = () => {
-    return (
-        <div>Example video 7:  hello world 33</div>
-    )
-}
-
-export default Video7;
--- src/examples/Video8.ts
+++
@@ -1,3 +0,0 @@
-let name1 = "hoi Khoa";
-name1 = '1'
-console.log(name1)
