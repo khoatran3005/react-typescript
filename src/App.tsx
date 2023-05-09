4131988ed0a59f65f68dbe6292ea15ee1c8ab4b9
Khoa Tran <khoatran3005@gmail.com>
2023-05-08 20:12:52 -0400

    error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Portfolio from './screens/Portfolio'
-
+import Portfolio from "./screens/Portfolio"
+import "@/styles/style.scss"
+import "@/styles/plugin.scss"
+  
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     // < !--WRAPPER ALL-- >
-    <Portfolio/>
+    <Portfolio />
   )
 }
 
